67d098020f0024f5efd3fa09a9918cd270ec1e53
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2026-03-29 19:41:59 -0400

    Update and rename sharks.js to games.js

---
+++ docker/routes/games.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const game = require('../controllers/sharks');
+
+router.get('/', function (req, res) {
+  game.index(req, res);
+});
+
+router.post('/addgame', function (req, res) {
+  game.create(req, res);
+});
+
+router.get('/getgame', function (req, res) {
+  game.list(req, res);
+});
+
+module.exports = router;
