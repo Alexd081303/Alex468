4716f7254713401dcff08b0187ba4798ea9edf82
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2026-04-28 12:48:40 -0400

    Update app.js

--- docker/app.js
+++ docker/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const db = require('./db');
-const games = require('./routes/sharks');
+const games = require('./routes/games');
 
 const path = __dirname + '/views/';
 const port = 8080;
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/games', games);
 
+app.get('/', (req, res) => {
+  res.sendFile(path + 'index.html');
+});
+
 app.listen(port, function () {
-  console.log('Game app listening on port 8080!');
+  console.log('Game app listening on port ' + port + '!');
 });
