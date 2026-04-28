28c864892313ae1322bdc890ab3a32d1906bbfb7
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2026-04-28 12:57:28 -0400

    Update db.js

--- docker/db.js
+++ docker/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
-
-const MONGO_HOSTNAME = 'mongo';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'sharkinfo';
-
+ 
+const MONGO_HOSTNAME = process.env.MONGO_HOSTNAME || 'mongo';
+const MONGO_PORT = process.env.MONGO_PORT || '27017';
+const MONGO_DB = process.env.MONGO_DB || 'sharkinfo';
+ 
 const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-
-mongoose.connect(url, { useNewUrlParser: true });
+ 
+mongoose.connect(url, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch(err => console.error('MongoDB connection error:', err));
