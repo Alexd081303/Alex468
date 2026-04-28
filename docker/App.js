267581f91c5081842c20d852b0525d5c13035eea
Alexd081303 <143731771+Alexd081303@users.noreply.github.com>
2026-04-28 13:05:10 -0400

    Create App.js

---
+++ docker/App.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const db = require('./db');
+const games = require('./routes/games');
+ 
+const path = __dirname + '/views/';
+const port = 8080;
+ 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path));
+app.use('/games', games);
+ 
+app.get('/', (req, res) => {
+  res.sendFile(path + 'index.html');
+});
+ 
+app.listen(port, function () {
+  console.log('Game app listening on port ' + port + '!');
+});
+ 
